15b4f94b0ff5d4e37a25e3738c6ea2ab3b68d030
Tamerlan <zanshugurov07@gmail.com>
2024-12-02 16:49:06 +0500

    feat: add layuot main page

---
+++ pages/index.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="row">
+    <div class="col-md-4">
+      <select class="form-select" aria-label="Default select example">
+        <option selected>Select jenre</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
+    </div>
+    <div class="col-md-4">
+      <select class="form-select" aria-label="Default select example">
+        <option selected>Select country</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
+
+    </div>
+    <div class="col-md-2">
+      <select class="form-select" aria-label="Default select example">
+        <option value="1">Name</option>
+        <option value="2">Year</option>
+        <option value="3">Rating</option>
+      </select>
+    </div>
+    <div class="col-md-2">
+      <button class="btn btn-outline-warning me-2" type="submit">Reset</button>
+    </div>
+  </div>
+  <div class="mt-4 row row-cols-1 row-cols-md-3 g-4">
+    <div class="col">
+      <div class="card">
+        <img src="https://avatars.mds.yandex.net/get-kinopoisk-image/4774061/cf1970bc-3f08-4e0e-a095-2fb57c3aa7c6/600x900" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Matrix</h5>
+          <p class="card-text">Rating:4.5</p>
+          <p class="card-text">150 min.</p>
+          <p class="card-text">Genre:Action, Fantastic</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+
+  </div>
+  <nav class="mt-4 d-flexjustify-content-center" aria-label="Page navigation example">
+    <ul class="pagination">
+      <li class="page-item">
+        <a class="page-link" href="#" aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>
+      <li class="page-item"><a class="page-link" href="#">1</a></li>
+      <li class="page-item"><a class="page-link" href="#">2</a></li>
+      <li class="page-item"><a class="page-link" href="#">3</a></li>
+      <li class="page-item">
+        <a class="page-link" href="#" aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+
+</template>
