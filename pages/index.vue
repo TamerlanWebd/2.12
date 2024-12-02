86376bb49af275f93e359d1431d80a65ef97dfd8
Tamerlan <zanshugurov07@gmail.com>
2024-12-02 18:56:55 +0500

    feat:categories 1 day

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,34 @@
 <script setup lang="ts">
+import {useFilmStore} from "~/stores/film";
+import {useCategoryStore} from "~/stores/category";
 
+const categoryStory = useCategoryStore();
+const filmStore = useFilmStore();
 </script>
 
-<template>
-  <div class="row">
-    <div class="col-md-4">
-      <select class="form-select" aria-label="Default select example">
-        <option selected>Select jenre</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
-      </select>
-    </div>
-    <div class="col-md-4">
-      <select class="form-select" aria-label="Default select example">
-        <option selected>Select country</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
-      </select>
 
+<template>
+
+  <div class="col-md-4">
+    <select class="form-select" aria-label="Default select example">
+      <option :value="null" selected>Select country</option>
+      <option
+          v-for="category in categoryStory.categories"
+          :key="category.id"
+          :value="category.id">{{ category.name }} ({{category.filmCount}}) </option>
+    </select>
+  </div>
+
+  <div class="row ">
+    <div class="col-md-4">
+      <select class="form-select" aria-label="Default select example">
+        <option selected>Выбор жанра</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
     </div>
+
     <div class="col-md-2">
       <select class="form-select" aria-label="Default select example">
         <option value="1">Name</option>
@@ -28,42 +36,31 @@
         <option value="3">Rating</option>
       </select>
     </div>
-    <div class="col-md-2">
-      <button class="btn btn-outline-warning me-2" type="submit">Reset</button>
+    <div class="col-md-2">   <button type="button" class="btn btn-outline-info">Reset</button></div>
+  </div>
+  <div v-if="!filmStore.isLoading"  class="d-flex justify-content-center">
+    <div  class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
     </div>
   </div>
-  <div class="mt-4 row row-cols-1 row-cols-md-3 g-4">
-    <div class="col">
-      <div class="card">
-        <img src="https://avatars.mds.yandex.net/get-kinopoisk-image/4774061/cf1970bc-3f08-4e0e-a095-2fb57c3aa7c6/600x900" class="card-img-top" alt="...">
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="col" v-for="film in filmStore.films" :key="film.id">
+      <div class="card h-100">
+        <img v-if="film.link_img" :src="film.link_img" class="card-img-top" alt="...">
+        <img v-else="film.link_img" src="https://artgallerynsk.ru/upload/iblock/25c/25c5bfba434540925e36313a39c6864e.jpg" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Matrix</h5>
-          <p class="card-text">Rating:4.5</p>
-          <p class="card-text">150 min.</p>
-          <p class="card-text">Genre:Action, Fantastic</p>
+          <h5 class="card-title"> {{ film.name}}</h5>
+          <p class="card-text"> {{ film.raringAvg}}</p>
+          <p class="card-text"> {{ film.duration }} min.</p>
+          <p class="card-text">
+            <template v-for="(category, index) in film.categories" :key="category.id">
+              {{ category.name + (index + 1 < film.categories.length ? ", ": "") }}
+            </template>
+          </p>
+
         </div>
       </div>
     </div>
   </div>
-  <div class="col">
-
-  </div>
-  <nav class="mt-4 d-flexjustify-content-center" aria-label="Page navigation example">
-    <ul class="pagination">
-      <li class="page-item">
-        <a class="page-link" href="#" aria-label="Previous">
-          <span aria-hidden="true">&laquo;</span>
-        </a>
-      </li>
-      <li class="page-item"><a class="page-link" href="#">1</a></li>
-      <li class="page-item"><a class="page-link" href="#">2</a></li>
-      <li class="page-item"><a class="page-link" href="#">3</a></li>
-      <li class="page-item">
-        <a class="page-link" href="#" aria-label="Next">
-          <span aria-hidden="true">&raquo;</span>
-        </a>
-      </li>
-    </ul>
-  </nav>
 
 </template>
