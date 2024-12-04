4ae22e504a3896d069dee9adeefa5f4a56631057
Tamerlan <zanshugurov07@gmail.com>
2024-12-04 18:39:26 +0500

    feat:add layout film detail

--- Layouts/default.vue
+++ Layouts/default.vue
@@ -8,7 +8,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="http://localhost:3000/">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
@@ -44,7 +44,7 @@
   </div>
   <footer class="bg-dark text-white p-3 mt-3">
     <div class="container text-center">
-      <p class="lead">&copy;Dominic</p>
+      <p class="lead">COPYRIGHT</p>
     </div>
   </footer>
 </template>
---
+++ pages/film/[id].vue
@@ -0,0 +1,93 @@
+<script setup>
+
+</script>
+<template>
+  <div class="row">
+    <div class="col-md-2">
+      <h2> 4.8 </h2> </div>
+    <div class="col-md-8">
+      <iframe width="750" height="450" src="https://r190110.yandexwebcache.org/sword-art-online/1/1.1080.6c0b064388d8fb67.mp4?hash1=41390d90c9a20070e57e479cfc0a0a6d&hash2=0eed7c305a9b4989e059708715e727ab"></iframe>
+    </div>
+    <div class="col-md-2">
+      <ul class="rating-stars">
+        <li class="star-item"><a href="" class="star-link" data-bs-toggle="tooltip" title="5"><i class="bi bi-star"></i></a></li>
+        <li class="star-item"><a href="" class="star-link" data-bs-toggle="tooltip" title="5"><i class="bi bi-star"></i></a></li>
+        <li class="star-item"><a href="" class="star-link" data-bs-toggle="tooltip" title="5"><i class="bi bi-star"></i></a></li>
+        <li class="star-item"><a href="" class="star-link" data-bs-toggle="tooltip" title="5"><i class="bi bi-star"></i></a></li>
+        <li class="star-item"><a href="" class="star-link" data-bs-toggle="tooltip" title="5"><i class="bi bi-star"></i></a></li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-12">
+      <h1>Matrix</h1>
+    </div>
+
+    <div class="col-md-8 fs-5">
+      120 min USA 1999  16+  03.12.2024 Action, Fantastic &nbsp
+    </div>
+
+    <div class="col-md-4-fs-4 text-end">
+      <a href="#">Kinopoisk</a>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-10">
+      <label for="exampleFormControlTextarea1" class="form-label">Add review</label>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+    <div class="col-md-2 d-flex flex-column-reverse">
+      <button class="btn btn-outline-success">Send</button>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-12 fs-4 mt-2">Reviews</div>
+    <div class="row">
+      <div class="col">
+        <div class="row ">
+          <div class="col-md-10 fs-5 fw-bold">Ivan</div>
+          <div class="col-md-2t ext-end">04.24.2024</div>
+        </div>
+        <div class="row">
+          <div class="col fs-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti dignissimos doloremque doloribus eaque esse fugiat fugit harum inventore laboriosam molestias nemo non numquam placeat, quas quo reiciendis repudiandae sunt ut?</div>
+        </div>
+      </div>
+      <div class="col-12">
+        <hr class="border border-success border-2 opacity-50">
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<style scoped>
+.rating-stars {
+  list-style-type: none;
+}
+.star-item {
+  font-size: 2.5rem;
+}
+.star-link {
+  color: #490495;
+}
+.star-link:hover {
+  color: #ff00cd;
+}
+</style>
